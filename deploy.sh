16e0c88868be600801892648d918ac034c8b78b9
Felix Arntz <felixarntz@google.com>
2019-02-28 16:55:15 +0100

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2019 Google LLC
+# Reporting API for WordPress, Copyright 2019 Google LLC
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
